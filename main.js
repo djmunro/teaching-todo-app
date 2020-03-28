038a7933b46701804059bb59a4626c4c476b0a0d
David Munro <david.munro@nokia.com>
2020-03-28 12:23:58 -0400

    Refactoring todo list

--- main.js
+++ main.js
@@ -11,10 +11,42 @@ function addTodo() {
   console.log("foo bar baz bah bing boo");
 
   // take the todo-input value and create a new <li> element
-  const todo = document.createElement("li");
-  todo.innerText = todoInput.value;
+  const todo = makeTodoListItem();
 
   // append the new list item to our todo list
   const todoList = document.querySelector(".todo-list");
   todoList.appendChild(todo);
+}
+
+function makeTodoListItem() {
+  // create list item
+  const todo = document.createElement("li");
+  todo.className = "todo-item";
+
+  // create todoText element
+  const todoText = document.createElement("span");
+  todoText.innerText = todoInput.value;
+
+  // creates deleteButton element
+  const deleteButton = document.createElement("button");
+  deleteButton.onclick = deleteTodo(todo);
+  deleteButton.className = "delete-todo";
+  deleteButton.innerText = "X";
+
+  // add todo text to list item
+  // add delete button to list item
+  todo.appendChild(todoText);
+  todo.appendChild(deleteButton);
+
+  return todo;
+}
+
+function deleteTodo() {
+  // create a function to delete the list item
+  /*
+    <li class="todo-item">
+      <span>Bar</span>
+      <button class="delete-todo">X</button>
+    </li>
+  */
 }
