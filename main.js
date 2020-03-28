a1842fdb65088362fcf4a8265259a234ef91d51a
David Munro <david.munro@nokia.com>
2020-03-28 11:48:22 -0400

    Refactoring

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // get the todo-input element
 const todoInput = document.querySelector(".todo-input");
-// console.log(todoInput.value);
 
 // get the todo-add-btn
 const addBtn = document.querySelector(".add-todo-btn");
@@ -10,6 +9,7 @@ addBtn.onclick = addTodo;
 
 function addTodo() {
   console.log("foo bar baz bah bing boo");
+
   // take the todo-input value and create a new <li> element
   const todo = document.createElement("li");
   todo.innerText = todoInput.value;
